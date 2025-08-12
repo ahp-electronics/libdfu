a04a0107c8cd1604e974226563f711c4c2e7e86a
Ilia Platone <info@iliaplatone.com>
2025-08-12 17:26:30 +0200

    Update FindUSB.cmake

--- cmake_modules/FindUSB.cmake
+++ cmake_modules/FindUSB.cmake
@@ -1,60 +1,83 @@
-# - Try to find USB
+# - Try to find libusb-1.0
 # Once done this will define
 #
-#  USB_FOUND - system has USB
-#  USB_INCLUDE_DIR - the USB include directory
-#  USB_LIBRARIES - Link these to use USB
-#  USB_VERSION_STRING - Human readable version number of cfitsio
-#  USB_VERSION_MAJOR  - Major version number of cfitsio
-#  USB_VERSION_MINOR  - Minor version number of cfitsio
-
-# Copyright (c) 2006, Jasem Mutlaq <mutlaqja@ikarustech.com>
-# Based on FindLibfacile by Carsten Niehaus, <cniehaus@gmx.de>
+#  USB1_FOUND - system has libusb-1.0
+#  USB1_INCLUDE_DIRS - the libusb-1.0 include directories
+#  USB1_LIBRARIES - Link these to use libusb-1.0
+#  USB1_DEFINITIONS - Compiler switches required for using libusb-1.0
 #
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+#  USB1_HAS_LIBUSB_ERROR_NAME - defined when libusb-1.0 has libusb_error_name()
 
-if (USB_INCLUDE_DIR AND USB_LIBRARIES)
+#=============================================================================
+# Copyright (c) 2017 Pino Toscano <toscano.pino@tiscali.it>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#=============================================================================
 
-  # in cache already
-  set(USB_FOUND TRUE)
-  message(STATUS "Found USB: ${USB_LIBRARIES}")
+find_package(PkgConfig)
+pkg_check_modules(PC_LIBUSB1 QUIET libusb-1.0)
 
+find_path(USB1_INCLUDE_DIR
+  NAMES
+    libusb.h
+  HINTS
+    ${PC_LIBUSB1_INCLUDE_DIRS}
+  PATH_SUFFIXES
+    libusb-1.0
+)
 
-else (USB_INCLUDE_DIR AND USB_LIBRARIES)
+find_library(USB1_LIBRARY
+  NAMES
+    ${PC_LIBUSB1_LIBRARIES}
+    usb-1.0
+  HINTS
+    ${PC_LIBUSB1_LIBRARY_DIRS}
+)
 
-  # JM: Packages from different distributions have different suffixes
-  find_path(USB_INCLUDE_DIR libusb.h
-    PATH_SUFFIXES libusb-1.0
-    ${_obIncDir}
-    ${GNUWIN32_DIR}/include
-  )
+set(USB1_INCLUDE_DIRS ${USB1_INCLUDE_DIR})
+set(USB1_LIBRARIES ${USB1_LIBRARY})
 
-  find_library(USB_LIBRARIES NAMES usb-1.0
-    PATHS
-    ${_obLinkDir}
-    ${GNUWIN32_DIR}/lib
-    HINTS ${CMAKE_C_IMPLICIT_LINK_DIRECTORIES}
-  )
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(USB1
+  FOUND_VAR
+    USB1_FOUND
+  REQUIRED_VARS
+    USB1_LIBRARY
+    USB1_INCLUDE_DIR
+  VERSION_VAR
+    PC_LIBUSB1_VERSION
+)
 
-  if(USB_INCLUDE_DIR AND USB_LIBRARIES)
-    set(USB_FOUND TRUE)
-  else (USB_INCLUDE_DIR AND USB_LIBRARIES)
-    set(USB_FOUND FALSE)
-  endif(USB_INCLUDE_DIR AND USB_LIBRARIES)
+mark_as_advanced(USB1_INCLUDE_DIRS USB1_LIBRARIES)
 
-
-  if (USB_FOUND)
-
-    if (NOT USB_FIND_QUIETLY)
-      message(STATUS "Found USB ${USB_VERSION_STRING}: ${USB_LIBRARIES}")
-    endif (NOT USB_FIND_QUIETLY)
-  else (USB_FOUND)
-    if (USB_FIND_REQUIRED)
-      message(STATUS "USB not found.")
-    endif (USB_FIND_REQUIRED)
-  endif (USB_FOUND)
-
-  mark_as_advanced(USB_INCLUDE_DIR USB_LIBRARIES)
-
-endif (USB_INCLUDE_DIR AND USB_LIBRARIES)
+if(USB1_FOUND)
+  include(CheckCXXSourceCompiles)
+  include(CMakePushCheckState)
+  cmake_push_check_state(RESET)
+  set(CMAKE_REQUIRED_INCLUDES ${USB1_INCLUDE_DIRS})
+  set(CMAKE_REQUIRED_LIBRARIES ${USB1_LIBRARIES})
+  check_cxx_source_compiles("#include <libusb.h>
+    int main() { libusb_error_name(0); return 0; }" USB1_HAS_LIBUSB_ERROR_NAME)
+  cmake_pop_check_state()
+endif()
